e90be2e3d2de5da719fa5f84c2197d8d98411e32
Jesse Frey <jmfrey@alaska.edu>
2019-05-09 06:21:10 -0600

    Update strip size for new split strip

--- LEDs.h
+++ LEDs.h
@@ -10,9 +10,9 @@
 
 void initLEDs(void);
 
-#define NUM_LEDS    600
+#define NUM_LEDS    (3*LED_LEN)
 
-#define LED_LEN     50
+#define LED_LEN     10
 
 #define MAX_BRT     0x1F
 #define LED_ST_BITS 0xE0
