c8aacbea12699a4470388a3ecda5715068766d50
Jesse Frey <jmfrey@alaska.edu>
2019-07-03 07:14:23 -0600

    Add pre-defined colors to flashPattern.h

--- flashPattern.h
+++ flashPattern.h
@@ -19,6 +19,19 @@
 #define LED_BRT_HIGH     (18)
 #define LED_BRT_EXHIGH   (24)
 
+//define standard colors                    brightness               | b  | g  | r  |
+#define LED_COLOR_RED           ((LED_color){LED_ST_BITS|LED_BRT_NORM,0x00,0xFF,0xFF})
+#define LED_COLOR_GREEN         ((LED_color){LED_ST_BITS|LED_BRT_NORM,0x00,0xFF,0x00})
+#define LED_COLOR_BLUE          ((LED_color){LED_ST_BITS|LED_BRT_NORM,0xFF,0x00,0x00})
+#define LED_COLOR_YELLOW        ((LED_color){LED_ST_BITS|LED_BRT_NORM,0x00,0xFF,0xFF})
+#define LED_COLOR_ORANGE        ((LED_color){LED_ST_BITS|LED_BRT_NORM,0x00,0xA5,0xFF})
+#define LED_COLOR_PURPLE        ((LED_color){LED_ST_BITS|LED_BRT_NORM,0x80,0x00,0x80})
+#define LED_COLOR_PINK          ((LED_color){LED_ST_BITS|LED_BRT_NORM,0xCB,0xC0,0xFF})
+
+
+#define LED_COLOR_WHITE         ((LED_color){LED_ST_BITS|LED_BRT_NORM,0xFF,0xFF,0xFF})
+
+
 void flashPatternChange(int pattern);
 void flashPatternAdvance(void);
 void HsvToLED(LED_color *dest,unsigned char brt,unsigned char hue,unsigned char saturation,unsigned char value);
