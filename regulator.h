903961251d339d8bc2ef06fcf298f0a6a299adab
Lisa Anders <lisa.anders@posit.co>
2023-09-24 14:42:30 -0600

    Fixes for panic loop (the nightknight must fly!)

--- regulator.h
+++ regulator.h
@@ -14,13 +14,13 @@ void reg5V_on(void);
 
 #define reg5V_off()     (P1OUT&=~BIT7)
 
-#define reg5V_is_on()   (P1OUT&BIT7)
+#define reg5V_is_on()   (P1OUT&BIT7)q
 
 extern int reg_flags;
 
 enum{REG_FLAGS_STARTUP=0x01,REG_FLAGS_ERROR=0x02,REG_FLAGS_DEGLITCH=0x04};
 
-#define REG_STARTUP_TICKS        (33)        //about 1 ms
+#define REG_STARTUP_TICKS        (800)        //about 24 ms
 #define REG_DEGLITCH_TICKS       (660)        //about 20 ms
 
 #endif /* REGULATOR_H_ */
